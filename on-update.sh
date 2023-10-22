f89686c2a59870fc255b5234356ea5089566cdb6
Ozeliurs <ozeliurs@gmail.com>
2023-10-22 18:23:55 +0200

    feat: update on-update command

--- on-update.sh
+++ on-update.sh
@@ -3,6 +3,7 @@ DOCKER_COMPOSE_VARIANT="docker-compose"
 
 DOCKER_COMPOSE_RESTART_COMMAND="$DOCKER_COMPOSE_VARIANT up -d --force-recreate --remove-orphans"
 
-# cd hello-world && $DOCKER_COMPOSE_RESTART_COMMAND && cd ..
+cd traefik && $DOCKER_COMPOSE_RESTART_COMMAND && cd ..
+cd authelia && $DOCKER_COMPOSE_RESTART_COMMAND && cd ..
 
 echo "Successfully updated!"
