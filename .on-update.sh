c3a5267b41eeb58fffc8d0932ad8fa925a136735
Ozeliurs <ozeliurs@gmail.com>
2023-11-24 18:32:31 +0100

    fix: remove authelia env and fix startup script

--- .on-update.sh
+++ .on-update.sh
@@ -3,7 +3,6 @@ DOCKER_COMPOSE_VARIANT="docker compose"
 
 DOCKER_COMPOSE_RESTART_COMMAND="$DOCKER_COMPOSE_VARIANT up -d --force-recreate --remove-orphans"
 
-# cd traefik && $DOCKER_COMPOSE_RESTART_COMMAND && cd ..
-cd authelia && $DOCKER_COMPOSE_RESTART_COMMAND && cd ..
+$DOCKER_COMPOSE_RESTART_COMMAND
 
 echo "Successfully updated!"
