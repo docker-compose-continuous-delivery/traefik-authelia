2f2820d747712b3fd8b5afcf1c71a4d6364e8e83
Ozeliurs <ozeliurs@gmail.com>
2023-11-24 18:53:12 +0100

    fix: authelia db

--- .on-update.sh
+++ .on-update.sh
@@ -3,6 +3,8 @@ DOCKER_COMPOSE_VARIANT="docker compose"
 
 DOCKER_COMPOSE_RESTART_COMMAND="$DOCKER_COMPOSE_VARIANT up -d --force-recreate --remove-orphans"
 
+touch authelia/db.sqlite3
+
 $DOCKER_COMPOSE_RESTART_COMMAND
 
 echo "Successfully updated!"
